122575db46fa49b1c679403754e5059b64d9a8e1
iskandarsaleh <nukamari08@gmail.com>
2024-05-26 20:32:09 +0700

    update runDocker

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker image build -t nukamari/umkmbooster .
-sudo docker container run -d -p 8082:80 ubuntu/nukamari/umkmbooster
+sudo docker container run -d -p 8082:80 ubuntu/nukamari/umkmbooster/nginx
