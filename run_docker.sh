4864979cc7cdd1ba3ab5a019f64a5cf734167ddf
iskandarsaleh <nukamari08@gmail.com>
2024-05-26 20:53:54 +0700

    update album to index

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker image build -t umkmbooster/nginx .
-sudo docker container run -d -p 8082:80 ubuntu/umkmbooster/nginx
+sudo docker container run -d -p 8082:80 umkmbooster/nginx
