17c8927301eb4717549c81cd1cd52d3e59231eb8
iskandarsaleh <nukamari08@gmail.com>
2024-05-26 20:20:05 +0700

    update dockerfile

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker image build -t nukamari/umkmbooster .
-sudo docker container run -d -p 8082:80 ubuntu/loanweb/nginx
+sudo docker container run -d -p 8082:80 ubuntu/nukamari/umkmbooster
